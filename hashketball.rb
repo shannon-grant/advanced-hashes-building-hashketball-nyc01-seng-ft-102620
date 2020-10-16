78acdfdb10e7ee14345033934edb822a42212400
Shannon Grant <grant.shannon90@gmail.com>
2020-10-16 16:03:59 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -4,7 +4,23 @@ def game_hash
     :home => {
       :team_name => "Brooklyn Nets",
       :colors => ["Black", "White"],
-      :players => [{}]
+      :players => [
+        { :player_name =>"Alan Anderson", :number =>, :shoe =>,
+          :points =>, :rebounds =>, :assists =>, :steals =>,
+          :blocks =>, :slam_dunks => }
+        { :player_name =>"Reggie Evans", :number =>, :shoe =>,
+          :points =>, :rebounds =>, :assists =>, :steals =>,
+          :blocks =>, :slam_dunks => }
+        { :player_name =>"Brook Lopez", :number =>, :shoe =>,
+          :points =>, :rebounds =>, :assists =>, :steals =>,
+          :blocks =>, :slam_dunks => }
+        { :player_name =>"Mason Plumlee", :number =>, :shoe =>,
+          :points =>, :rebounds =>, :assists =>, :steals =>,
+          :blocks =>, :slam_dunks => }
+        { :player_name =>"Jason Terry", :number =>, :shoe =>,
+          :points =>, :rebounds =>, :assists =>, :steals =>,
+          :blocks =>, :slam_dunks => } 
+      ]
     },
     :away => {
       :team_name => "Charlotte Hornets",
